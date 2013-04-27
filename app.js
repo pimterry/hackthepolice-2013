5ceb7f41dcd3a4eec90ef0f83f218bca198a7271
Tim Perry <pimterry@gmail.com>
2013-04-27 17:11:14 +0100

    Show location error data

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function onDeviceReady() {
           getLocation(function (location) {
             self.location(ko.toJSON(location));
           }, function (error) {
-            alert("Error recording location");
+            alert("Error recording location: " + error);
             self.location(undefined);
           });
           self.isRecorded(true);
